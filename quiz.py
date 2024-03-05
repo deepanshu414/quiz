7502119e44a32bc11bb0463c6fcbe121ddb5f600
deepanshu414 <131351308+deepanshu414@users.noreply.github.com>
2024-03-05 12:24:26 +0530

    quiz.py

--- quiz.py
+++ quiz.py
@@ -76,12 +76,12 @@ if(st.button("Submit")):
     elif(main==""):
         st.warning("Please enter your name")
     else:
-                st.markdown("""<style>
-                        button.st-emotion-cache-7ym5gk.ef3psqc12 {
-                            display: none;
-                        }
-                        </style>""",True)
-                l.append(datetime.now())
+        st.markdown("""<style>
+            button.st-emotion-cache-7ym5gk.ef3psqc12 {
+            display: none;
+            }
+            </style>""",True)
+        l.append(datetime.now())
         marks={q1:"15",q2:"3",q3:"32",q4:"24",q5:"5",q6:"class",q7:"String",q8:"x is greater",q9:"static",q10:"A package can be defined within another package",q11:"Hello",q12:"int [][] arr = new int[3][3]",q13:"0 to 255",q14:"1variable",q15:"Depends on the data type",q16:"true",q17:"4 bytes",q18:"float",q19:"false",q20:"char",q21:"8 bytes",q22:"-32768 to 32767",q23:"0"}
         count=0
         for key,value in marks.items():
